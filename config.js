15181d679f674e4ab9e097bff7b80f1c37c84038
ConvitePremium <essenciamusicalofc@gmail.com>
2026-09-15 15:12:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -87,10 +87,10 @@ window.CONFIG = {
 
   // ----- BOTÕES DE VOLTAR -------------------------------------------------
   // mostrarTexto: true mostra “Voltar”; false deixa apenas a área clicável.
-  // A posição pode ser alterada no editor ?editor=1.
-  botoesVoltar: {
+  // A posição pode ser alterada no editor ?editor=1
+    botoesVoltar: {
     presentes: { mostrarTexto:false, posicao: {"left":30.538670626996808,"top":86.82210659607239,"width":38.64217252396166,"height":4.923130085037458} },
-    dresscode: { mostrarTexto:false, posicao: {"left":27.214392596845045,"top":89.87744510457583,"width":46.581485248602235,"height":5.424627114446271} }
+    dresscode: { mostrarTexto:false, posicao: {"left":27.214392596845045,"top":89.87744510457583,"width":46.581485248602235,"height":5.424627114446271} },
   },
 
   // ----- POSIÇÃO DOS BOTÕES NA TELA PRINCIPAL ----------------------------
